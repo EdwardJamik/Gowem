cf593c415b9049c34d39091718864e8f97cddb06
Eduard Babetskyi <edward.babetskyi@gmail.com>
2025-07-17 03:28:36 +0200

    fixed import command.js

--- packages/gowem/index.js
+++ packages/gowem/index.js
@@ -1,17 +1,20 @@
-require('./command.js');
+require('./commands.js');
 
-mp.events.add('playerJoin', (player) => {
-    player.spawn(new mp.Vector3(-425.517, 1123.620, 325.854)); // приклад координат
-    player.outputChatBox('Ласкаво просимо на сервер! Машина біля тебе.');
+mp.events.add('playerJoin', player => {
+	player.spawn(new mp.Vector3(-425.517, 1123.62, 325.854)); // приклад координат
+	player.outputChatBox('Ласкаво просимо на сервер! Машина біля тебе.');
 });
 
 // Створюємо машину біля точки спавну
 mp.vehicles.new(
-    mp.joaat('infernus'), // модель машини (Infernus)
-    new mp.Vector3(-430.0, 1120.0, 325.0), // координати біля спавну
-    {
-        heading: 90,      // кут повороту
-        color: [[255, 0, 0], [0, 0, 0]], // кольори
-        numberPlate: 'TEST'
-    }
+	mp.joaat('infernus'), // модель машини (Infernus)
+	new mp.Vector3(-430.0, 1120.0, 325.0), // координати біля спавну
+	{
+		heading: 90, // кут повороту
+		color: [
+			[255, 0, 0],
+			[0, 0, 0],
+		], // кольори
+		numberPlate: 'TEST',
+	}
 );
